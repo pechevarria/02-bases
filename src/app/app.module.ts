25e03a93d11d018cc41a65a9938f4f29ba1c17e8
pechevarria <pechevarria05@gmail.com>
2023-08-05 03:10:52 -0500

    modulo dbz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CounterModule } from './counter/components/counter.module';
 import { heroesModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CounterModule, heroesModule],
+  imports: [BrowserModule, CounterModule, heroesModule, DbzModule],
   providers: [],
   bootstrap: [AppComponent],
 })
