8870cdc7c9754295d3fdd1982e1b88b738dc4fa8
pechevarria <pechevarria05@gmail.com>
2023-08-07 02:53:10 -0500

    finalizacion del curso

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'dbz-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss'],
 })
-export class ListComponent {}
+export class ListComponent {
+  @Input()
+  public characterList: Character[] = [
+    {
+      name: 'Trunks',
+      power: 10,
+    },
+  ];
+
+  @Output()
+  public onDelete: EventEmitter<string> = new EventEmitter();
+
+  onDeleteCharacter(id?: string): void {
+    //TODO: Emitir el ID del Personaje
+    this.onDelete.emit(id);
+  }
+}
